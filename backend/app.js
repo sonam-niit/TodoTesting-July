89699275d6b81555b6f41b30af45a30f55f29547
sonam-niit <sonam.gravity@gmail.com>
2024-12-24 12:25:27 +0530

    mongo url updated

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const app = require('./server');
 const mongoose= require('mongoose');
 mongoose.connect
-    ('mongodb+srv://sonamsoni:5bJqTeOHj6e4jwVH@cluster0.e5c2s4d.mongodb.net/todoapp')
+    ('mongodb//localhost:27017/todoapp')
     .then(resp => console.log('connected'))
     .catch(error => console.error('Error while connecting DB'))
 
